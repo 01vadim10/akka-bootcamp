d0b49b54ed756e474b93bb138bbd476e28a1839c
Vadim Zylevich <vzy@leapwork.com>
2019-08-27 08:41:14 +0300

    Unit 3. Lesson 3

--- src/Unit-3/DoThis/Actors/GithubCommanderActor.cs
+++ src/Unit-3/DoThis/Actors/GithubCommanderActor.cs
@@ -68,7 +68,8 @@ namespace GithubActors.Actors
         private void BecomeAsking()
         {
             _canAcceptJobSender = Sender;
-            pendingJobReplies = 3;
+            pendingJobReplies = _coordinator.Ask<Routees>(new GetRoutees())
+                .Result.Members.Count();
             Become(Asking);
         }
 
@@ -106,18 +107,9 @@ namespace GithubActors.Actors
 
         protected override void PreStart()
         {
-            var c1 = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()),
-                ActorPaths.GithubCoordinatorActor.Name + "1");
-            var c2 = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()),
-                ActorPaths.GithubCoordinatorActor.Name + "2");
-            var c3 = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()),
-                ActorPaths.GithubCoordinatorActor.Name + "3");
-
-            _coordinator = Context.ActorOf(Props.Empty.WithRouter(
-                new BroadcastGroup(
-                    ActorPaths.GithubCoordinatorActor.Path + "1",
-                    ActorPaths.GithubCoordinatorActor.Path + "2",
-                    ActorPaths.GithubCoordinatorActor.Path + "3")));
+            _coordinator = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor())
+                .WithRouter(FromConfig.Instance),
+                ActorPaths.GithubCoordinatorActor.Name);
             base.PreStart();
         }
 
